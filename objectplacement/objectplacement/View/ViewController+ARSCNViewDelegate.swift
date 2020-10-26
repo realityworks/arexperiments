decfce47a921cee2a54e9d553cd33c9b1c40b987
Peter Suwara <psuwara@realityworks.com.au>
2020-10-26 19:59:29 +0300

    Comments for clarity

--- objectplacement/objectplacement/View/ViewController+ARSCNViewDelegate.swift
+++ objectplacement/objectplacement/View/ViewController+ARSCNViewDelegate.swift
@@ -10,7 +10,15 @@ import SceneKit
 import ARKit
 
 extension ViewController : ARSCNViewDelegate {
+    /// Callback when a node has been added
+    /// - Parameters:
+    ///   - renderer: Renderer used by the scene
+    ///   - node: Node that  has been added
+    ///   - anchor: Attached anchor
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        // We want to know if we have a new plane anchor detected upon which we can start
+        // Casting rays
         guard anchor is ARPlaneAnchor else { return }
         
         viewModel.canPlaceObject.send(true)
