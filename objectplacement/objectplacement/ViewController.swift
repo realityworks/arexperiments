5fe8346549da0486a9d9b253fafb5e9a3e79c1ac
Peter Suwara <psuwara@realityworks.com.au>
2020-10-26 06:22:04 +0300

    objectplacement

---
+++ objectplacement/objectplacement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  objectplacement
+//
+//  Created by Piotr Suwara on 26/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
