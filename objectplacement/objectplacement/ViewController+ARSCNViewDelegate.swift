e60d449344bf74af5ca01cbea727e9ebc67a12da
Peter Suwara <psuwara@realityworks.com.au>
2020-10-26 07:05:18 +0300

    Added swift pm and tiny constraints

---
+++ objectplacement/objectplacement/ViewController+ARSCNViewDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController+ARSCNViewDelegate.swift
+//  objectplacement
+//
+//  Created by Piotr Suwara on 26/10/20.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+
+extension ViewController : ARSCNViewDelegate {
+    
+}
--- objectplacement/objectplacement/ViewController.swift
+++ objectplacement/objectplacement/ViewController.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+import SceneKit
+import ARKit
+import TinyConstraints
 
 class ViewController: UIViewController {
+    
+    // MARK: UI Connections
+    var sceneView: ARSCNView = ARSCNView(frame: .zero)
+    
+    // MARK: AR Properties
+    let worldTrackingConfiguration = ARWorldTrackingConfiguration()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        /// Just use tiny constraints here to add the subview instead of having to add IB contraints
+        self.view.addSubview(sceneView)
+        sceneView.edgesToSuperview()
     }
-
-
 }
 
